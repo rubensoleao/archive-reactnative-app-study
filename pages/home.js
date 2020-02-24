a3d005342dffa34d5b4255f2ab3399cb5497986c
Rubens Leao <rubensoleao@gmail.com>
2020-02-24 17:19:58 -0300

    Adicionado textinputs e botões da parte do editor. Nao compila

--- pages/home.js
+++ pages/home.js
@@ -8,22 +8,218 @@
 //VVVVVVV
 import React from 'react';
 import { StyleSheet,
-         Text,
-         View,
-         Button } from 'react-native';
+Text,
+View,
+Button,
+ScrollView,
+TextInput,
+KeyboardAvoidingView,
+SafeAreaView
+} from 'react-native';
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+
+//Biblioteca Formic usado para preenchimento de dados
+import { Formik } from 'formik';
 
 
 
 export default function HomeScreen({navigation}){
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-		<Text>Home Screen</Text>
-		<Button
-			title="Ir para tela de edição"
-			onPress={() => navigation.navigate('Editor')}
-		/>
+return (
+<KeyboardAvoidingView style={styles.conteiner} behavior="padding">
+
+	<ScrollView>
+		<Text style={[styles.largeText, styles.textStyle]}> Cadastro de Alunos</Text>
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Nome"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+
 		</View>
-	);
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Ano do colegio"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Rua"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Numero"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Complemento"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+
+			<TextInput
+
+
+			placeholder="Bairro"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Cidade"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+			<TextInput
+
+
+			placeholder="Estado"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+
+			<TextInput
+
+
+			placeholder="Nome Da Mãe"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+
+			<TextInput
+
+
+			placeholder="CPF da Mãe"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+
+		<View style={styles.conteinerTextIput}>
+
+			<TextInput
+
+
+			placeholder="Data preferencial de Pagamento"
+
+			placeholderTextColor="white"
+			underlineColorAndroid="transparent"
+			style={styles.textInput}
+			/>
+		</View>
+
+		<Text style="{height:20}"></Text>
+
+		<Button title='Cadastrar Aluno'  />
+
+		<Button
+		title="Ir para tela de edição"
+		onPress={() => navigation.navigate('Editor')}
+		/>
+
+
+	</ScrollView>
+</KeyboardAvoidingView >
+);
 }
+
+
+const styles = StyleSheet.create({
+conteiner: {
+flex: 1,
+backgroundColor: 'white',
+},
+conteinerTextInput: {
+height: 40,
+marginTop: 20,
+backgroundColor: '#6685B3',
+marginHorizontal: 40,
+paddingHorizontal: 10,
+borderRadius: 5,
+},
+textInput: {
+marginTop:5,
+color: 'white',
+fontWeight: 'bold',
+},  
+textStyle: {
+textAlign: 'center',
+fontFamily: 'Roboto',
+},
+largeText: {
+fontSize: 44,
+},
+tinyText: {
+
+fontSize: 11,
+textAlign:'left',
+}
+});
