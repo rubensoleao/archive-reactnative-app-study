b7b078838da3fd45064b85b01a7aa48de5af1d9c
Rubens Leao <rubensoleao@gmail.com>
2020-02-26 12:25:36 -0300

    Adicionada função gravarAlunos para edição
    Função recebe um perfil de aluno carrega o banco de dados em uma variavel, a atualiza com valor novo.
    
    Ainda não funciona para alunos novos

--- src/bd.js
+++ src/bd.js
@@ -16,14 +16,14 @@ const  masterKey = 'MasterBD'
 // Armazena aluno no banco de dados.
 // Recebe Perfil do aluno e utilza sua propriedade id para criação da chave
 //
-export  const gravarAluno = async () => {
+export  const gravarAluno = async (perfilAluno) => {
+	//console.log('Iniciado Fç gravar alunos')
     // primeiramente lê o banco de dados de alunos
     try {
     	var dbAlunos = await AsyncStorage.getItem(masterKey)
 		if (dbAlunos){
 			//Foi lido com sucesso o Banco de Dados
 			dbAlunos = JSON.parse(dbAlunos);
-			console.log(dbAlunos);
 		} else{
 			console.log('DB lida com sucesso mas dbAlunos retornou NULL');
 		}
@@ -31,11 +31,20 @@ export  const gravarAluno = async () => {
 	catch(error){
 		console.log('erro ao ler BD em função gravarAluno' + error);
 	}
-    //try {
-    //	await AsyncStorage.setItem(value.id, JSON.stringify(value));
-    //} catch (error) {
-    //	console.log("Error ao escrever dados" + error);
-    //}
+	//console.log('dbLido:')
+	//console.log(dbAlunos)
+	//console.log('A escrever:')
+	//console.log(perfilAluno);
+	//adiciona aluno ao dbGravado na variavel
+	dbAlunos[perfilAluno.id] = perfilAluno;
+	//console.log('dbatualizado:')
+	//console.log(dbAlunos)
+	//escreve a db devolta na memoria local
+    try {
+    	await AsyncStorage.setItem(masterKey, JSON.stringify(dbAlunos));
+    } catch (error) {
+    	console.log("Error ao escrever dados" + error);
+    }
 }
 
 
@@ -49,11 +58,13 @@ export  const lerAlunos = async ()=> {
 }
 
 export const initDB = async ()=> {
-	const dbInicial = [
-	{id:'xxasd1294c92180498120m9c291m',Nome: 'Antonio José Munia',DataNascimento:'22/05/1999', Cidade: 'São Jõao do Mato verde' , Ano: '1', Rua:'Rua José Pereira Manuel Antonio',Numero:'12',Complemento:'FRENTE',Bairro:'Jd. Primavera',Cidade:'São jõao do Mato Bento',Estado:'SP', CEP:'150599', NomeDaMae:'Maria Julia Casanova',CPFdaMae:'364.123.123-12',DataPagamento:'5',},
-	{id:'dalklq12lk3123l1k231lskl',Nome: 'Antonio Roger Braga',DataNascimento:'22/05/1999', Cidade: 'São Jõao do Mato verde' , Ano: '1', Rua:'Rua José Pereira Manuel Antonio',Numero:'12',Complemento:'FRENTE',Bairro:'Jd. Primavera',Cidade:'São jõao do Mato Bento',Estado:'SP', CEP:'150599', NomeDaMae:'Maria Julia Casanova',CPFdaMae:'364.123.123-12',DataPagamento:'5',},
-	{id:'23k41j2lk3j41l2kj34lk1jk4j',Nome: 'São Jõao de Nunes Feição Penhoso',DataNascimento:'22/05/1999', Cidade: 'São Jõao do Mato verde' , Ano: '1', Rua:'Rua José Pereira Manuel Antonio',Numero:'12',Complemento:'FRENTE',Bairro:'Jd. Primavera',Cidade:'São jõao do Mato Bento',Estado:'SP', CEP:'150599', NomeDaMae:'Maria Julia Casanova',CPFdaMae:'364.123.123-12',DataPagamento:'5',},
-	];
+	const dbInicial = {aluno_xxasdasdas:{id:'aluno_xxasdasdas',Nome: 'Antonio José Munia',DataNascimento:'22/05/1999', Cidade: 'São Jõao do Mato verde' , Ano: '1', Rua:'Rua José Pereira Manuel Antonio',Numero:'12',Complemento:'FRENTE',Bairro:'Jd. Primavera',Cidade:'São jõao do Mato Bento',Estado:'SP', CEP:'150599', NomeDaMae:'Maria Julia Casanova',CPFdaMae:'364.123.123-12',DataPagamento:'5',
+	},
+	 aluno_dalklq12lk3123l1k231lskl:{id:'aluno_dalklq12lk3123l1k231lskl',Nome: 'Antonio Roger Braga',DataNascimento:'22/05/1999', Cidade: 'São Jõao do Mato verde' , Ano: '1', Rua:'Rua José Pereira Manuel Antonio',Numero:'12',Complemento:'FRENTE',Bairro:'Jd. Primavera',Cidade:'São jõao do Mato Bento',Estado:'SP', CEP:'150599', NomeDaMae:'Maria Julia Casanova',CPFdaMae:'364.123.123-12',DataPagamento:'5',
+	},
+	 aluno_23k41j2lk3j41l2kj34lk1jk4j:{id:'aluno_23k41j2lk3j41l2kj34lk1jk4j',Nome: 'São Jõao de Nunes Feição Penhoso',DataNascimento:'22/05/1999', Cidade: 'São Jõao do Mato verde' , Ano: '1', Rua:'Rua José Pereira Manuel Antonio',Numero:'12',Complemento:'FRENTE',Bairro:'Jd. Primavera',Cidade:'São jõao do Mato Bento',Estado:'SP', CEP:'150599', NomeDaMae:'Maria Julia Casanova',CPFdaMae:'364.123.123-12',DataPagamento:'5',
+	},
+};
 	try {
 
 		// Tenta ver se o BD esta iniciado
